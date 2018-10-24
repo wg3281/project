5a3c62b1c07ad2e59702b0ba1fca9f8323c641bd
wg3281 <wg3281@hotmail.com>
2018-10-24 15:17:46 +0800

    a

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            1
+            int i = 123;
+            object o = i;
+            int j = (int)o;
 
         }
     }
