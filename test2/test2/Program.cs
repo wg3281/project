35cc389d9ecb8c93346b9c9fa6fc2d131fd98795
wg3281 <wg3281@hotmail.com>
2018-10-29 11:40:11 +0800

    a

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -13,10 +13,16 @@ namespace test2
             child XiaoMing = new child();//实例化对象
             XiaoMing.Name = "马小明";
             //XiaoMing.Sex = "男";
-            XiaoMing.Age = 6;
+            XiaoMing.Age = 3;
             XiaoMing.Height = 120;
-            Console.WriteLine("我今年"+XiaoMing.Age+"岁");
+            //Console.WriteLine("我今年"+XiaoMing.Age+"岁");
             //XiaoMing.PlayBall();
+            //XiaoMing.EatSugar("牛奶糖");
+            //XiaoMing.EatSugar(4);
+            //XiaoMing.EatSugar("水果糖",2);
+            //XiaoMing.EatSugar();
+            XiaoMing.Add(3,7);
+            Console.WriteLine($"和是{XiaoMing.Add(3, 7)}");
             Console.Read();
         }
     }
--- test2/test2/child.cs
+++ test2/test2/child.cs
@@ -28,7 +28,7 @@ namespace test2
             get => _age;
             set
             {
-                if(value>=3&&value<=7)
+                if (value >= 3 && value <= 7)
                     _age = value;
             }
         }
@@ -37,11 +37,89 @@ namespace test2
         /// <summary>
         /// 踢足球
         /// </summary>
-        public void PlayBall()
+        public void PlayBall()//方法声明
         {
             Console.WriteLine("耶！我是小小C罗");
         }
+        /// <summary>
+        /// 吃糖
+        /// </summary>
+        /// <param name="sugar">糖的名称</param>
+        public void EatSugar(string sugar)
+        {
+            if (sugar == "榴莲糖")
+            {
+                Console.WriteLine("哎呀，最怕榴莲糖");
+            }
+            else
+            {
+                Console.WriteLine("哈哈，是我最喜欢的糖");
+            }
 
+        }
+        /// <summary>
+        /// 吃糖
+        /// </summary>
+        /// <param name="count">糖的数量</param>
+        public void EatSugar(int count)
+        {
+            if (count > 3)
+            {
+                Console.WriteLine("吃糖太多对牙齿不好");
+            }
+            else
+            {
+                Console.WriteLine("吃吧，吃糖糖吧");
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sugar"></param>
+        /// <param name="count"></param>
+        public void EatSugar(string sugar, int count)
+        {
+            if (sugar == "牛奶糖" && count > 2)
+            {
+                Console.WriteLine("牛奶糖不能吃太多");
+            }
+            else if (count > 3)
+            {
+                Console.WriteLine("糖不能吃太多");
+            }
+            else
+            {
+                Console.WriteLine("吃糖糖吧");
+            }
+        }
+        public void EatSugar()
+        {
+            if (Age < 4)
+            {
+                Console.WriteLine("年龄太小，不能吃糖");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("我喜欢吃棉花糖");
+                Console.WriteLine("我喜欢吃泡泡糖");
+                Console.WriteLine("我喜欢吃水果糖");
+                Console.WriteLine("我喜欢吃牛奶糖");
+                Console.WriteLine("我喜欢吃太妃糖");
+                Console.WriteLine("我喜欢吃巧克力糖");
+                Console.WriteLine("我喜欢吃高粱糖");
+            }
+        }
+        /// <summary>
+        /// 求和
+        /// </summary>
+        /// <param name="n1">加数1</param>
+        /// <param name="n2">加数2</param>
+        public int Add(int n1, int n2)
+        {
+            int sum = n1 + n2;
+            return sum;//返回值
+        }
     }
 
 }
