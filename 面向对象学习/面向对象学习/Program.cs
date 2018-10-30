e7f1dd7bdfc4891a8b5ec581b4d878737e7995f1
wg3281 <wg3281@hotmail.com>
2018-10-30 16:38:58 +0800

    a

--- 面向对象学习/面向对象学习/Program.cs
+++ 面向对象学习/面向对象学习/Program.cs
@@ -9,38 +9,128 @@ namespace 面向对象学习
     /// <summary>
     /// 宠物父类
     /// </summary>
-    public class Pet
+    struct Fish
     {
-        public string Name;
-        public uint Age;
+        int weigth;
+        int size;
+        int type;
+    }
+    abstract public class Pet
+    {
 
+        protected string _name;
+        public uint Age;
+        public Pet(string name)
+        {
+            _name = name;
+        }
         public void PrintName()
         {
-            Console.WriteLine("Pet's name is "+Name);
+            Console.WriteLine("Pet's name is " + _name);
+        }
+        virtual public void Speak()
+        {
+
+        }
+
+    }
+    interface ICatchMice
+    {
+        void CatchMice();
+    }
+    interface IClimbTree
+    {
+        void Climbtree();
+    }
+    static class PetGuide
+    {
+        static public void HowToFeedDog(this Dog dog)
+        {
+            Console.WriteLine("Play a vedio about how to feed a dog.");
         }
     }
     public class Dog : Pet
     {
-         new public void PrintName()
+        static int Num=0;
+        static Dog()
         {
-            Console.WriteLine("宠物名字是 " + Name);
+
+        }
+        public Dog(string name):base(name)
+        {
+            ++Num;
+        }
+        public new void PrintName()
+        {
+            Console.WriteLine("宠物名字是 " + _name);
+        }
+        public sealed override void Speak()
+        {
+            Console.WriteLine($"{_name} is speaking wangwang");
+        }
+        static public void ShowNum()
+        {
+            Console.WriteLine(Num);
         }
     }
-    public class Cat : Pet
+    public class Cat : Pet,ICatchMice,IClimbTree
     {
+        public Cat(string name) : base(name)
+        {
+            
+        }
 
+        public void CatchMice()
+        {
+            Console.WriteLine($"{_name} catched Mice");
+        }
+
+        public void Climbtree()
+        {
+            Console.WriteLine($"{_name} climbed tree");
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine($"{_name} is speaking wow");
+        }
+    }
+    public class Labrador : Dog
+    {
+        public Labrador(string name) : base(name)
+        {
+
+        }
+       
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Dog dog = new Dog();
-            dog.Name = "Jack";
-            dog.PrintName();
-            Cat cat = new Cat();
-            cat.Name = "Tom";
-            cat.PrintName();
-
+            Pet[] pets = new Pet[] { new Dog("Jack"), new Cat("Tom"),new Dog("Jerry") };
+            for (var i=0 ; i < pets.Length; i++)
+            {
+                pets[i].Speak();
+            }
+            Cat c1 = new Cat("Tom1");
+            c1.CatchMice();
+            c1.Climbtree();
+            IClimbTree climb = (IClimbTree)c1;
+            climb.Climbtree();
+            ICatchMice catchMice = (ICatchMice)c1;
+            catchMice.CatchMice();
+            Dog.ShowNum();
+            Dog dog = new Dog("Tommy");
+            dog.HowToFeedDog();
+            {
+                int i = 3;
+                object oi = i;
+                Console.WriteLine($"i={i} oi={oi}");
+                oi = 10;
+                Console.WriteLine($"i={i} oi={oi}");
+                int j = (int)oi;
+                Console.WriteLine("j=" + j);
+            }
         }
     }
 }
